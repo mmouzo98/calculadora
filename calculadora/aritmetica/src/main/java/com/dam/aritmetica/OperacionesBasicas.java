f1f5e710306950a5ab22caea314229888c444b70
wadmin <wadmin@base>
2021-01-25 08:23:45 +0100

    ola

---
+++ calculadora/aritmetica/src/main/java/com/dam/aritmetica/OperacionesBasicas.java
@@ -0,0 +1,28 @@
+package com.dam.aritmetica;
+
+public class OperacionesBasicas {
+
+	public static int suma(int a, int b)
+	{
+		return a+b;
+	}
+	
+	public static int resta(int a, int b)
+	{
+		return a-b;
+	}
+	
+	public static int multiplicacion(int a, int b)
+	{
+		return a*b;
+	}
+	
+	public static int division(int a, int b)
+	{
+		return a/b;
+	}
+	
+	public static double raizCuadrada(double a) {
+		return Math.sqrt(a);
+	}
+}
---
+++ calculadora/src/main/java/com/dam/calculadora/Main.java
@@ -0,0 +1,68 @@
+package com.dam.calculadora;
+
+import java.util.Scanner;
+
+import com.dam.aritmetica.OperacionesBasicas;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner teclado = new Scanner(System.in);
+
+		while (true) {
+			System.out.println("CALCULADORA");
+			System.out.println("1.- SUMA.");
+			System.out.println("2.- RESTA.");
+			System.out.println("3.- MULTIPLICACION");
+			System.out.println("4.- DIVISION");
+			System.out.println("5.- RAIZ CUADRADA");
+			System.out.println("6.- AREA DE UN CÍRCULO");
+			System.out.println("0.- SALIR");
+			System.out.println("INTRODUZCA OPCION");
+
+			int opcion = teclado.nextInt();
+			System.out.println("Introduzca valor a:");
+			int a = teclado.nextInt();
+			System.out.println("Introduzca valor b:");
+			int b = teclado.nextInt();
+
+			switch (opcion) {
+			case 1:
+				System.out.println("SUMA:");
+				System.out.println(OperacionesBasicas.suma(a, b));
+				break;
+			case 2:
+				System.out.println("RESTA:");
+				System.out.println(OperacionesBasicas.resta(a, b));
+				break;
+			case 3:
+				System.out.println("MULTIPLICACION:");
+				System.out.println(OperacionesBasicas.multiplicacion(a, b));
+				break;
+			case 4:
+				System.out.println("DIVISION:");
+				System.out.println(OperacionesBasicas.division(a, b));
+				break;
+
+			case 5:
+				System.out.println("RAIZ CUADRADA");
+				System.out.println(OperacionesBasicas.raizCuadrada(a));
+
+			case 6:
+				System.out.println("ÁREA DE UN CÍRCULO:");
+				System.out.println(OperacionesBasicas.areaCirculo(a, b));
+				break;
+			case 7:
+				System.out.println("AREA DEL TRIANGULO");
+				System.out.println(OperacionesBasicas.areatri(a, b));
+				break;
+			case 0:
+				return;
+			}
+
+		}
+
+	}
+
+}
